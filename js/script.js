20b0de3a9d961d600d21427f12b88f2946531db1
Ahmed Abdelaziz <a.abdelaziz44@gmail.com>
2016-10-08 13:07:37 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+
+
+$(function() {
+    
+    
+    
+    
+    //Active Class
+    $('.links li').click(function() {
+        
+        $(this).addClass('active').siblings().removeClass('active');
+        
+    });
+    
+    
+    
+    
+    
+});
